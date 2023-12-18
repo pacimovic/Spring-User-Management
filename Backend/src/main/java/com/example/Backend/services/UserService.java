4e35daf6d298563398da11ec7f989a779d1699e0
petar <petaracimovic990@gmail.com>
2023-12-18 15:06:35 +0100

    Added JWT Filter and Util

--- Backend/src/main/java/com/example/Backend/services/UserService.java
+++ Backend/src/main/java/com/example/Backend/services/UserService.java
@@ -2,14 +2,18 @@ package com.example.Backend.services;
 
 import com.example.Backend.model.User;
 import com.example.Backend.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 
 @Service
-public class UserService implements IService<User, Long>{
+public class UserService implements IService<User, Long>, UserDetailsService {
 
     private final UserRepository userRepository;
 
@@ -35,5 +39,16 @@ public class UserService implements IService<User, Long>{
     @Override
     public void deleteById(Long var1) {
 
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User myUser = this.userRepository.findByEmail(email);
+        if(myUser == null) {
+            throw new UsernameNotFoundException("Email: "+email+" not found");
+        }
+
+        return new org.springframework.security.core.userdetails.User(myUser.getEmail(), myUser.getPassword(), new ArrayList<>());
     }
 }
