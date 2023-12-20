c78cbe62c5d63f9ccf2f5c3d98b4acd389d3bf90
petar <petaracimovic990@gmail.com>
2023-12-20 09:49:37 +0100

    Added permission to Login Response

--- Backend/src/main/java/com/example/Backend/controllers/AuthController.java
+++ Backend/src/main/java/com/example/Backend/controllers/AuthController.java
@@ -1,6 +1,7 @@
 package com.example.Backend.controllers;
 
 
+import com.example.Backend.model.Permission;
 import com.example.Backend.requests.LoginRequest;
 import com.example.Backend.responses.LoginResponse;
 import com.example.Backend.services.UserService;
@@ -8,6 +9,8 @@ import com.example.Backend.utils.JwtUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -27,13 +30,24 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest){
+        Authentication au;
         try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+            au = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
         } catch (Exception e){
             e.printStackTrace();
             return ResponseEntity.status(401).build();
         }
 
-        return ResponseEntity.ok(new LoginResponse(jwtUtil.generateToken(loginRequest.getEmail())));
+        Permission permission = new Permission();
+        if(au.getAuthorities().contains(new SimpleGrantedAuthority("can_read_users"))) permission.setCan_read_users(true);
+        else permission.setCan_read_users(false);
+        if(au.getAuthorities().contains(new SimpleGrantedAuthority("can_create_users"))) permission.setCan_create_users(true);
+        else permission.setCan_create_users(false);
+        if(au.getAuthorities().contains(new SimpleGrantedAuthority("can_update_users"))) permission.setCan_update_users(true);
+        else permission.setCan_update_users(false);
+        if(au.getAuthorities().contains(new SimpleGrantedAuthority("can_delete_users"))) permission.setCan_delete_users(true);
+        else permission.setCan_delete_users(false);
+
+        return ResponseEntity.ok(new LoginResponse(jwtUtil.generateToken(loginRequest.getEmail()), permission));
     }
 }
